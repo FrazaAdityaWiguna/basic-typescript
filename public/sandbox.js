6ddeff9af7b0d2c9b7b523f5405b8fb6de35da00
FrazaAdityaWiguna <adityafraja12@gmail.com>
2022-12-21 22:36:32 +0700

    feat: func signature

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,10 +1,22 @@
 "use strict";
-const logDetails = (uid, item) => {
-    console.log(`${item} has a uid ${uid}`);
+// let greet = Function;
+// example 1
+let greet;
+greet = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
 };
-const greet = (user) => {
-    console.log(`${user.name} says hello`);
+// example 2
+let calc;
+calc = (numberOne, numberTwo, action) => {
+    if (action === "add") {
+        return numberOne + numberTwo;
+    }
+    else {
+        return numberOne - numberTwo;
+    }
 };
-const greetAgain = (user) => {
-    console.log(`${user.name} says hello`);
+// example 3
+let logDetails;
+logDetails = (person) => {
+    console.log(`${person.name} is ${person.age} years old`);
 };
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,14 +1,28 @@
-type StringOrNum = string | number;
-type objWithName = { name: string; uid: StringOrNum };
+// let greet = Function;
 
-const logDetails = (uid: StringOrNum, item: string) => {
-  console.log(`${item} has a uid ${uid}`);
+// example 1
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
 };
 
-const greet = (user: objWithName) => {
-  console.log(`${user.name} says hello`);
+// example 2
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numberOne: number, numberTwo: number, action: string) => {
+  if (action === "add") {
+    return numberOne + numberTwo;
+  } else {
+    return numberOne - numberTwo;
+  }
 };
 
-const greetAgain = (user: objWithName) => {
-  console.log(`${user.name} says hello`);
+// example 3
+let logDetails: (obj: { name: string; age: number }) => void;
+
+type tempPerson = { name: string; age: number };
+
+logDetails = (person: tempPerson) => {
+  console.log(`${person.name} is ${person.age} years old`);
 };
