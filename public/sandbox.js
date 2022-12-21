3916d9277756ec8111a5d7f526e9ac9d49f84760
FrazaAdityaWiguna <adityafraja12@gmail.com>
2022-12-21 22:03:35 +0700

    feat: function basic

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("test");
+let greet;
+greet = () => {
+    console.log("Hello, Indonesia");
+};
+const add = (a, b, c) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10);
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(15, 10);
+console.log(result);
+result = "test";
+console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,20 @@
-console.log("test");
+let greet: Function;
+
+greet = () => {
+  console.log("Hello, Indonesia");
+};
+
+const add = (a: number, b: number, c?: number | string) => {
+  console.log(a + b);
+  console.log(c);
+};
+add(5, 10);
+
+const minus = (a: number, b: number): any => {
+  return a - b;
+};
+
+let result = minus(15, 10);
+console.log(result);
+result = "test";
+console.log(result);
