2ba9a30ce70e39c8f7aa2d5ac703c3cbef4b3d6c
FrazaAdityaWiguna <adityafraja12@gmail.com>
2022-12-21 21:17:03 +0700

    feat: array and object

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,21 @@
-var character = "Nada", age = 19, isBlackBelt = false;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// arrays
+// array just contain string
+var names = ["Nada", "Arrasy", "Tegar"];
+names.push("Raja");
+// arrays just contain number
+var nums = [9, 12, 6];
+nums.push(10);
+// arrays just contain string and number
+var mixed = ["Batam", 12, "Tangerang", 09];
+mixed.push("Bali");
+mixed.push(30);
+mixed[0] = 912;
+// Objects
+var person = {
+    name: "Nada",
+    height: 155,
+    age: 22
 };
-console.log(circ(18));
+person.name = "Fraza";
+person.height = 174;
+person.age = 20;
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,25 @@
-let character = "Nada",
-  age = 19,
-  isBlackBelt = false;
+// arrays
+// array just contain string
+let names = ["Nada", "Arrasy", "Tegar"];
+names.push("Raja");
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
+// arrays just contain number
+let nums = [9, 12, 6];
+nums.push(10);
+
+// arrays just contain string and number
+let mixed = ["Batam", 12, "Tangerang", 09];
+mixed.push("Bali");
+mixed.push(30);
+mixed[0] = 912;
+
+// Objects
+let person = {
+  name: "Nada",
+  height: 155,
+  age: 22,
 };
 
-console.log(circ(18));
+person.name = "Fraza";
+person.height = 174;
+person.age = 20;
