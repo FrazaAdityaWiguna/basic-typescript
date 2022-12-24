0ef997f142e8063353b59fa0605fdb115cc28068
FrazaAdityaWiguna <adityafraja12@gmail.com>
2022-12-24 22:33:48 +0700

    feat: modules

--- public/app.js
+++ public/app.js
@@ -1,18 +1,4 @@
-"use strict";
-// classes
-class Invoice {
-    // readonly client: string;
-    // private details: string;
-    // public amount: number;
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from "./classes/Invoice.js";
 const invOne = new Invoice("Nada", "work on Nada website", 250);
 const invTwo = new Invoice("Fraza", "work on Fraza website", 300);
 let invoices = [];
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,4 @@
-// classes
-class Invoice {
-  // readonly client: string;
-  // private details: string;
-  // public amount: number;
-
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number
-  ) {}
-
-  format() {
-    return `${this.client} owes $${this.amount} for ${this.details}`;
-  }
-}
+import { Invoice } from "./classes/Invoice.js";
 
 const invOne = new Invoice("Nada", "work on Nada website", 250);
 const invTwo = new Invoice("Fraza", "work on Fraza website", 300);
